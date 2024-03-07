c96d52229c5eae07666e959f45e422cb93d897f5
Mayank Pawar <mayankpawar2612@gmail.com>
2024-03-07 21:34:24 +0530

    added html routes

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return render_template('index.html')
+    return render_template('main.html')
+
+
+@app.route('/items')
+def market():
+    return render_template('items.html')
+
+
+@app.route('/cart')
+def news():
+    return render_template('cart.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
